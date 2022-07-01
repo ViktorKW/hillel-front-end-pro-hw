f8af47b70f6ec8354165d483068f0ffb61b59258
ViktorKW <viktorkw1201@gmail.com>
2022-07-01 11:39:02 +0300

    completed task

---
+++ script.js
@@ -0,0 +1,53 @@
+// //5
+// let total = 0;
+// const cycles = 500;
+// for (let i = 1; i <= cycles; i++) {
+//   total += i;
+//   console.log(`Average of ${total} in cycle ${i} = ${total / i}`);
+// }
+
+// //6
+// let total2 = 0;
+// for (let i = 30; i <= 80; i++) {
+//   if (i % 2 == 0) {
+//     total2 += i;
+//     console.log(total2);
+//   }
+// }
+
+// alert(total2);
+
+// //7
+// let total3 = 0;
+// for (let i = 100; i <= 200; i++) {
+//   if (i % 3 == 0) {
+//     console.log(i);
+//   }
+// }
+
+// //8
+// const num = Math.round(+prompt("Type num"));
+
+// let even_divider_amount = 0;
+// let even_divider_sum = 0;
+
+// for (let i = 1; i <= num; i++) {
+//   if (num % i == 0) {
+//     console.log(`Divider ${i}`);
+//     if (i % 2 == 0) {
+//       even_divider_amount++;
+//       even_divider_sum += i;
+//     }
+//   }
+// }
+
+// console.log(
+//   `Total even dividers = ${even_divider_amount}\nTotal sum of even dividers = ${even_divider_sum}`
+// );
+
+// //9
+// for (let i = 1; i <= 10; i++) {
+//   for (let j = 1; j <= 10; j++) {
+//     console.log(`${i} * ${j} = ${i * j}`);
+//   }
+// }
