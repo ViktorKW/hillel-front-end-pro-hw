3cdc949e1712045c22cea7c60d717e44c8135b42
ViktorKW <viktorkw1201@gmail.com>
2022-07-01 11:34:59 +0300

    completed task

---
+++ script.js
@@ -0,0 +1,39 @@
+//1
+// let left_operand = Number(prompt("Type first num"));
+// let right_operand = Number(prompt("Type second num"));
+// alert(`${left_operand} + ${right_operand}= ${left_operand + right_operand}\n
+// ${left_operand} - ${right_operand}= ${left_operand - right_operand}\n
+// ${left_operand} / ${right_operand}= ${left_operand / right_operand}\n
+// ${left_operand} * ${right_operand}= ${left_operand * right_operand}\n`);
+
+//2
+// alert(
+//   `${Number(prompt("Type first num")) === Number(prompt("Type second num"))}`
+// );
+
+//3
+// alert(
+//   `${
+//     (Number(prompt("Type first num")) +
+//       Number(prompt("Type second num")) +
+//       Number(prompt("Type third num"))) /
+//     3
+//   }`
+// );
+
+//4
+// alert(
+//   `${[prompt("Enter first str:")]} ${prompt("Enter second str:")} ${prompt(
+//     "Enter third str:"
+//   )}`
+// );
+
+//5
+// let number = prompt("Enter five-digit number: ");
+// alert(
+//   `${number} = ${Math.floor((number / 10000) % 10)} ${Math.floor(
+//     (number / 1000) % 10
+//   )} ${Math.floor((number / 100) % 10)} ${Math.floor((number / 10) % 10)} ${
+//     number % 10
+//   }`
+// );
