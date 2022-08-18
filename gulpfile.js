7a555131a6602873727104d28c1ed53ea8dbf969
ViktorKW <viktorkw1201@gmail.com>
2022-08-18 19:00:58 +0300

    added gulp jquery

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,11 @@ function minCSS(cb) {
 }
 
 function minJS(cb) {
-  src(["node_modules/lightslider/dist/js/lightslider.js", "src/js/*.js"])
+  src([
+    "node_modules/lightslider/dist/js/lightslider.js",
+    "src/js/*.js",
+    "node_modules/jquery/dist/jquery.js",
+  ])
     .pipe(concat("script.min.js"))
     .pipe(uglify())
     .pipe(dest("dist/js/"));
