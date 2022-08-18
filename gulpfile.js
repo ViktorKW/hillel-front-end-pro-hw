0681587fc3825204b934a86182366eb1d2d601fe
ViktorKW <viktorkw1201@gmail.com>
2022-08-18 18:46:31 +0300

    learning gulp

---
+++ gulpfile.js
@@ -0,0 +1,35 @@
+const { src, dest, series } = require("gulp");
+const concat = require("gulp-concat");
+const uglify = require("gulp-uglify");
+const imagemin = require("gulp-imagemin");
+const htmlmin = require("gulp-htmlmin");
+
+function minCSS(cb) {
+  src(["src/css/*.css", "node_modules/lightslider/dist/css/lightslider.css"])
+    .pipe(concat("style.min.css"))
+    .pipe(dest("dist/css/"));
+  cb();
+}
+
+function minJS(cb) {
+  src(["node_modules/lightslider/dist/js/lightslider.js", "src/js/*.js"])
+    .pipe(concat("script.min.js"))
+    .pipe(uglify())
+    .pipe(dest("dist/js/"));
+  cb();
+}
+
+function minImg(cb) {
+  src(["src/img/*", "node_modules/lightslider/dist/img/*"])
+    .pipe(imagemin())
+    .pipe(dest("dist/img"));
+  cb();
+}
+
+function minHtml(cb) {
+  src("src/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(dest("dist"));
+  cb();
+}
+exports.default = series(minCSS, minJS, minImg, minHtml);
