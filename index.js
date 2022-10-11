cd7e3dd4aee51f88e9cf30543d39bdac43ff56eb
ViktorKW <viktorkw1201@gmail.com>
2022-10-11 23:22:50 +0300

    added chains to navigation bar

--- index.js
+++ index.js
@@ -1,77 +1,21 @@
-// const show_all_pokemons_btn = document.querySelector(".show-all-pokemons");
+const show_all_pokemons_btn = document.querySelector(".show-all-pokemons");
 
-// show_all_pokemons_btn.addEventListener("click", () => {
-//   navigation_title.innerHTML = "All Pokemons";
+show_all_pokemons_btn.addEventListener("click", () => {
+  navigation_title.innerHTML = "All Pokemons";
 
-//   drawContentNavigation(`${default_url}/pokemon/`);
-//   drawPokemonInfo(`${default_url}/pokemon/1`);
+  drawContentNavigation(`${default_url}/pokemon/`);
+  drawPokemonInfo(`${default_url}/pokemon/1`);
 
-//   initPagination();
-// });
+  initPagination();
+});
 
-// const show_all_evolutions_btn = document.querySelector(".show-all-evolutions");
+const show_all_evolutions_btn = document.querySelector(".show-all-evolutions");
 
-// show_all_evolutions_btn.addEventListener("click", () => {
-//   navigation_title.innerHTML = "All Evolution Chains";
-//   drawContentNavigationEvolution(`${default_url}/evolution-chain/`);
-//   // drawContentNavigation(`${default_url}/evolution-chain/`);
-//   // drawPokemonInfo(`${default_url}/evolution-chain/1`);
+show_all_evolutions_btn.addEventListener("click", () => {
+  navigation_title.innerHTML = "All Evolution Chains";
+  drawContentNavigationEvolution(`${default_url}/evolution-chain/`);
+  // drawContentNavigation(`${default_url}/evolution-chain/`);
+  // drawPokemonInfo(`${default_url}/evolution-chain/1`);
 
-//   // initPagination();
-// });
-
-// async function getPokemon(url) {
-//   const res = await fetch(url);
-//   const data = await res.json();
-//   console.log(data);
-//   data.results.forEach((element) => {
-//     const getChains = async (url) => {
-//       const res = await fetch(element.url);
-//       const obj = await res.json();
-
-//       const cycleChains = async (obj) => {
-//         // console.log(obj.chain.evolves_to);
-//         if (obj.chain.evolves_to.length > 0) {
-//           await cycleChains(obj.chain.evolves_to[0]);
-//         } else {
-//           return obj.chain.species.name + ",";
-//         }
-//       };
-
-//       const mystrings = await cycleChains(obj);
-//       console.log(mystrings);
-//     };
-//     getChains();
-//   });
-// }
-
-const fetchData = (url, callback) =>
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      console.log(data);
-      callback(data);
-    });
-
-fetchData("https://pokeapi.co/api/v2/pokemon/bulbasaur", () => {});
-
-function getPokemon(base_url) {
-  fetchData(base_url, ({ results }) => {
-    results.forEach(({ url }) => {
-      fetchData(url, ({ chain }) => {
-        const loop = (obj) => {
-          console.log("Looping in", obj.species.name);
-          let result = obj.species.name + " ";
-          if (obj.evolves_to.length > 0) {
-            result += loop(obj.evolves_to[0]);
-          }
-          return result;
-        };
-
-        console.log(loop(chain));
-      });
-    });
-  });
-}
-
-getPokemon("https://pokeapi.co/api/v2/evolution-chain");
+  // initPagination();
+});
--- js/fetchData.js
+++ js/fetchData.js
@@ -7,29 +7,3 @@ const fetchData = (url, callback) =>
       console.log(data);
       callback(data);
     });
-
-// const asyncFetchData = async (url) =>
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => data);
-
-// const asyncFetchData = async (url) => {
-//   try {
-//     const resp = await fetch(url);
-//     if (!resp.ok) {
-//       throw new Error("errrrrrror");
-//     }
-//     const data = await resp.json();
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-// console.log("dodo: ", asyncFetchData(default_url));
-
-// const dodo = async () => {
-//   let pokemon;
-//   pokemon = await getPokemon();
-//   console.log("finsl", pokemon);
-//   return pokemon;
-// };
