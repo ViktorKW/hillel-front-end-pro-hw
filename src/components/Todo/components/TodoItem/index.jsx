08c879dfb27301707a465150f58b715d5bcc1ebd
ViktorKW <viktorkw1201@gmail.com>
2022-11-09 16:26:19 +0200

    added local storage

--- src/components/Todo/components/TodoItem/index.jsx
+++ src/components/Todo/components/TodoItem/index.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import "./style.scss";
 function TodoItem({ todo }) {
-  console.log(todo.description);
   return (
     <li className="todo-item">
       <p className="todo-description">{todo.description}</p>
--- src/components/Todo/index.jsx
+++ src/components/Todo/index.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import TodoForm from "./components/TodoForm";
 import TodoList from "./components/TodoList";
 
+const LOCAL_STORAGE_KEY = "react-todo-list-todos";
+
 function Todo() {
   const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    const storageTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+    if (storageTodos) {
+      setTodos(storageTodos);
+    }
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  }, [todos]);
 
   function addTodo(todo) {
     setTodos([todo, ...todos]);
