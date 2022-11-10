aede03153ff3dedcb9704b6ec418d2a6f5327c7c
ViktorKW <viktorkw1201@gmail.com>
2022-11-10 13:08:39 +0200

    changed from class to className

--- src/components/Todo/components/TodoList/TodoList.jsx
+++ src/components/Todo/components/TodoList/TodoList.jsx
@@ -4,7 +4,7 @@ import TodoItem from '../TodoItem/TodoItem';
 
 function TodoList({ todos, toggleTodo, removeTodo }) {
   return (
-    <ul class='todo-list'>
+    <ul className='todo-list'>
       {todos.map((todo) => (
         <TodoItem
           key={todo.id}
