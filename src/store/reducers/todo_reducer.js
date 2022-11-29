2358d15712cf8805da7a5775c3c3db9f39b4be55
ViktorKW <viktorkw1201@gmail.com>
2022-11-29 23:47:06 +0200

    changed back ADD_TODO order

--- src/store/reducers/todo_reducer.js
+++ src/store/reducers/todo_reducer.js
@@ -13,7 +13,7 @@ const initialState = {
 export default function todosReducer(state = initialState, { type, payload }) {
   switch (type) {
     case ADD_TODO:
-      return { ...state, todos: [payload, ...state.todos] };
+      return { ...state, todos: [...state.todos, payload] };
 
     case INIT_TODOS:
       return { ...state, todos: [...payload] };
