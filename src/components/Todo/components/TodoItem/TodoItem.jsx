1223147da1740b8d21d42b7ae8ef498d63e21537
ViktorKW <viktorkw1201@gmail.com>
2022-11-30 03:08:14 +0200

    added Toggle Button and some icons

--- src/components/Todo/components/TodoItem/TodoItem.jsx
+++ src/components/Todo/components/TodoItem/TodoItem.jsx
@@ -1,24 +1,46 @@
 import React from 'react';
 import { Button, ListItemButton, ListItemText } from '@mui/material';
-
+import { Check, DeleteOutline } from '@mui/icons-material';
 import './style.scss';
-import { Link, Outlet } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 function TodoItem({ todo, toggleTodo, removeTodo }) {
   function handleRemoveClick() {
     removeTodo(todo.id);
   }
+  function handleToggleClick() {
+    toggleTodo(todo.id);
+  }
   const path = `${todo.id}`;
+  const empty_space = (
+    <canvas className='empty-space' width={'10px'} height={'0px'}></canvas>
+  );
   return (
     <ListItemButton className='todo-item'>
       <Link className='todo-description' to={path}>
         <ListItemText>{todo.task}</ListItemText>
       </Link>
 
-      <canvas className='empty-space' width={'10px'} height={'0px'}></canvas>
-
-      <Button variant='contained' onClick={handleRemoveClick}>
-        Delete
-      </Button>
+      <div className='todo-menu'>
+        <Check
+          style={
+            todo.complited
+              ? { visibility: 'visible' }
+              : { visibility: 'hidden' }
+          }
+        />
+        {empty_space}
+        <Button variant='contained' onClick={handleToggleClick}>
+          Toggle
+        </Button>
+        {empty_space}
+        <Button
+          variant='outlined'
+          onClick={handleRemoveClick}
+          startIcon={<DeleteOutline />}
+        >
+          Delete
+        </Button>
+      </div>
     </ListItemButton>
   );
 }
