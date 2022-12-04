ac6f90cbca31ef608ab82d7a990fdd775b073ff7
ViktorKW <viktorkw1201@gmail.com>
2022-12-05 00:28:52 +0200

    added postlist

--- src/components/Posts/PostsList.jsx
+++ src/components/Posts/PostsList.jsx
@@ -1,7 +1,13 @@
-import React from 'react';
-import PostItem from './PostItem';
-
-export default function PostsList({ posts }) {
+import React, { useEffect } from 'react';
+import PostItem from './PostItem/PostItem';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchAllPosts } from '../../store/posts/postsSlice';
+export default function PostsList() {
+  const posts = useSelector((state) => state.posts.posts);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllPosts());
+  }, []);
   return (
     <ul className='posts-list'>
       {posts.map((post) => (
