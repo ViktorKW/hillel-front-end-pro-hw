b083508bd4565bb1bfa0541021d31718eed58929
ViktorKW <viktorkw1201@gmail.com>
2022-12-12 17:30:00 +0200

    added author to Comment

--- src/components/Posts/Comment/Comment.jsx
+++ src/components/Posts/Comment/Comment.jsx
@@ -1,6 +1,11 @@
-import './style.scss';
+import './Comment.scss';
 import React from 'react';
 
-export default function Comment({ comment }) {
-  return <div className='comment'>{comment.body}</div>;
+export default function Comment({ comment, author }) {
+  return (
+    <div className='comment-conteiner'>
+      <div className='author-info'>Author: {author}</div>
+      <div className='comment'>{comment.body}</div>
+    </div>
+  );
 }
