4fcdcfe14e414f40cf6fc88de86c17d635552d37
ViktorKW <viktorkw1201@gmail.com>
2022-12-27 20:54:31 +0200

    added form validation test

--- src/__test__/Todo.test.js
+++ src/__test__/Todo.test.js
@@ -9,21 +9,21 @@ beforeEach(() => {
 
 afterEach(cleanup);
 
+function addTodoTest(description) {
+  const inputTextBox = screen.getByTestId('todo-form-input');
+  const submitButton = screen.getByTestId('todo-form-submit');
+
+  fireEvent.change(inputTextBox, {
+    target: { value: description },
+  });
+  fireEvent.click(submitButton);
+}
+
 describe('Todo test active and inactive class styles', () => {
-  function addTodoToDOM(description) {
-    const inputElement = screen.getByTestId('todo-form-input');
-    const submitElement = screen.getByTestId('todo-form-submit');
-
-    fireEvent.change(inputElement, {
-      target: { value: description },
-    });
-    fireEvent.click(submitElement);
-  }
-
   const todo_test_description = 'Test class style';
 
   it('should have inactive class style when TodoItem initially rendered', async () => {
-    addTodoToDOM(todo_test_description);
+    addTodoTest(todo_test_description);
 
     const todo_element = screen.getByText(todo_test_description);
     expect(todo_element).toHaveClass('inactive');
@@ -36,5 +36,18 @@ describe('Todo test active and inactive class styles', () => {
 
     expect(todo_element).toHaveClass('active');
     expect(todo_element).not.toHaveClass('inactive');
+  });
+});
+
+describe('Todo validate form input data', () => {
+  it('should disable submit button and apply "error" style if form passing empty string', () => {
+    const empty_string = '';
+    addTodoTest(empty_string);
+
+    const inputTextBox = screen.getByTestId('todo-form-input');
+    const submitButton = screen.getByTestId('todo-form-submit');
+
+    expect(inputTextBox).toHaveClass('error');
+    expect(submitButton).toBeDisabled();
   });
 });
