361d1fd2720f65a1dff5d7dc13ef4b909eb32cb3
ViktorKW <viktorkw1201@gmail.com>
2023-01-01 12:44:08 +0200

    added BrowserRouter and some minor fixes

--- src/__test__/test-utils.js
+++ src/__test__/test-utils.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import todosReducer from '../store/todos/todosSlice';
+import { BrowserRouter } from 'react-router-dom';
+import { setupStore } from '../store';
 
 export function renderWithProviders(
   ui,
   {
     preloadedState = {},
 
-    store = configureStore({
-      reducer: { todos: todosReducer },
-      preloadedState,
-    }),
+    store = setupStore(preloadedState),
     ...renderOptions
   } = {}
 ) {
   function Wrapper({ children }) {
-    return <Provider store={store}>{children}</Provider>;
+    return (
+      <Provider store={store}>
+        <BrowserRouter>{children}</BrowserRouter>
+      </Provider>
+    );
   }
 
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
